4b84f2b72dad2a433c7ca661fbb46ff71d577776
dzony6969 <dzony6969@users.noreply.github.com>
2018-05-19 15:04:18 +0200

    add features for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import moment from 'moment'
+import numeral from 'numeral'
+
 
 const ExpenseListItem = ({description, amount, createdAt, id }) => (
     <div>
      <Link to={`/edit/${id}`} >
       <h2>{description}</h2>
       </Link>
-      <p> {amount} - {createdAt} </p>
+      <p> 
+    {numeral(amount /100).format('0,0.00') + 'zł '}
+       - 
+    {moment(createdAt).format('Do MMMM, YYYY')}
+      
+      </p>
       
     </div>
 );
